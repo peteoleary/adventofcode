c49277e93356de7e5518294fa9b358ef25120053
Peter O'Leary <pete@mudflapinc.com>
2024-12-28 15:46:43 -0800

    Simpler, correct implementation

--- 2024/day_5/src/main.rs
+++ 2024/day_5/src/main.rs
@@ -1,30 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 use petgraph::graphmap::GraphMap;
 
+type Dag = HashMap<u32, Vec<u32>>;
+
 // return true if there exists a path from start to end going only in the forward direction
-fn is_before(dag: &GraphMap<u32, (), petgraph::Directed>, start: u32, end: u32) -> bool {
-    let mut visited: HashMap<u32, bool> = HashMap::new();
-    let mut stack: Vec<u32> = vec![start];
-    while stack.len() > 0 {
-        let node = stack.pop().unwrap();
-        if node == end {
-            return true;
-        }
-        if visited.contains_key(&node) {
-            continue;
-        }
-        else {        
-            visited.insert(node, true);
-            for neighbor in dag.neighbors_directed(node, petgraph::Direction::Outgoing) {
-                stack.push(neighbor);
-            }
-        }
+fn is_before(dag: &Dag, start: u32, end: u32) -> bool {
+    match dag.get(&start) {
+        Some(value) => {
+            value.contains(&end)
+        },
+        None => false
     }
-    return false;
 }
 
 // return true if head is before all elements in tail
-fn is_before_list(dag: &GraphMap<u32, (), petgraph::Directed>, before: u32, after: Vec<u32>) -> bool {
+fn is_before_list(dag: &Dag, before: u32, after: Vec<u32>) -> bool {
     // println!("Checking if {} is before {:?}", before, after);
     for node in after {
         if !is_before(dag, before, node) {
@@ -34,7 +24,7 @@ fn is_before_list(dag: &GraphMap<u32, (), petgraph::Directed>, before: u32, afte
     return true;
 }
 
-fn is_after_list(dag: &GraphMap<u32, (), petgraph::Directed>, after: u32, before: Vec<u32>) -> bool {
+fn is_after_list(dag: &Dag, after: u32, before: Vec<u32>) -> bool {
     // println!("Checking if {} is after {:?}", after, before);
     for node in before {
         if !is_before(dag, node, after) {
@@ -44,7 +34,7 @@ fn is_after_list(dag: &GraphMap<u32, (), petgraph::Directed>, after: u32, before
     return true;
 }
 
-fn correct_updates(dag: &GraphMap<u32, (), petgraph::Directed>, updates: &Vec<u32>) -> bool {
+fn correct_updates(dag: &Dag, updates: &Vec<u32>) -> bool {
     for i in 0..updates.len() {
         let current = updates[i];
         if !is_before_list(dag, current, updates[i+1..].to_vec()) {
@@ -63,8 +53,9 @@ fn main() {
     println!("Hello, world!");
 
     let mut reading_edges = true;
-    let mut dag: GraphMap<u32, (), petgraph::Directed> = GraphMap::<u32, (), petgraph::Directed>::new();
-    let mut sum = 0;
+    let mut dag= Dag::new();
+    let mut correct_sum = 0;
+    let mut incorrect_sum = 0;
     for line in lines {
         if line.len() == 0 {
             reading_edges = false;
@@ -74,24 +65,30 @@ fn main() {
             let parts: Vec<&str> = line.split("|").collect();
             let parent: u32 = parts[0].parse().unwrap();
             let child: u32 = parts[1].parse().unwrap();
-            if !dag.contains_node(parent) {
-                dag.add_node(parent);
-            }
-            if !dag.contains_node(child) {
-                dag.add_node(child);
-            }
-            if !dag.contains_edge(parent, child) {
-                dag.add_edge(parent, child, ());
+            if dag.contains_key(&parent) {
+                dag.get_mut(&parent).unwrap().push(child);
+            } else {
+                dag.insert(parent, vec![child]);
             }
         } else { // reading updates
-            let updates = line.split(",").map(|x| x.parse().unwrap()).collect();
+            let mut updates = line.split(",").map(|x| x.parse().unwrap()).collect();
             if correct_updates(&dag, &updates) {
                 println!("Correct updates: {:?}", updates);
-                sum += updates[updates.len() / 2]
+                correct_sum += updates[updates.len() / 2]
             } else {
-                println!("Incorrect updates");
+                updates.sort_by(|a, b| {
+                    if is_before(&dag, *a, *b)
+                    {
+                        std::cmp::Ordering::Less
+                    } else {
+                        std::cmp::Ordering::Greater
+                    }
+                });
+                println!("Incorrect updates: {:?}", updates);
+                incorrect_sum += updates[updates.len() / 2]
             }
         }
     }
-    println!("Sum: {}", sum);
+    println!("Correct Sum: {}", correct_sum);
+    println!("Incorrect Sum: {}", incorrect_sum);
 }
