6c4f11377596c7182c2546cb6df7deabba4de82f
Peter O'Leary <pete@mudflapinc.com>
2024-12-31 06:58:44 -0800

    Finished part 2

--- 2024/day_7/src/main.rs
+++ 2024/day_7/src/main.rs
@@ -1,5 +1,7 @@
 use std::result;
 
+use big_int::prelude::*;
+
 mod binary_tree;
 
 fn generate_bitmaps(n: usize) -> Vec<Vec<bool>> {
@@ -14,15 +16,38 @@ fn generate_bitmaps(n: usize) -> Vec<Vec<bool>> {
     bitmaps
 }
 
+// this can be easily generalized into a function that generates for any base
+fn generate_tripmaps(n: usize) -> Vec<Vec<u32>> {
+    let mut tripmaps = Vec::new();
+    let total_len = usize::pow(3, n as u32);
+    let input: Vec<Tight<3>> = (0..total_len).map(|v| v.try_into().unwrap()).collect::<Vec<Tight<3>>>();
+    for i in 0..total_len {
+        tripmaps.push(Vec::new());
+        let mut input_string = input[i].to_string();
+        if input_string.len() < n {
+            input_string = "0".repeat(n - input_string.len()) + &input_string;
+        }
+        input_string.chars().for_each(|c| {
+            tripmaps[i].push(c.to_digit(10).unwrap());
+        });
+    }
+    tripmaps
+}
+
 fn try_evals(nums: Vec<i32>, target: i64) -> bool {
     println!("try_evals: {} {:?}", target, nums);
-    for operators in generate_bitmaps(nums.len() - 1) {
+    for operators in generate_tripmaps(nums.len() - 1) {
         let result: i64 = nums[1..].iter().zip(operators.iter()).fold(nums[0].into(), |acc, (num, op)| {
-            if *op {
+            if *op == 0 {
                 acc + i64::from(*num)
-            } else {
+            } else if *op == 1 {
                 acc * i64::from(*num)
-            }
+            } else {
+                let mut new_string = acc.to_string();
+                new_string.push_str((*num).to_string().as_str());
+                new_string.parse::<i64>().unwrap()
+                
+            } 
         });
         if result == target {
             println!("found: {:?} {:?}", nums, target);
@@ -84,5 +109,29 @@ mod tests {
     #[test]
     fn test_try_evals() {
         assert_eq!(try_evals(vec![10, 19], 190), true);
+    }
+    #[test]
+    fn test_generate_tripmaps_1() {
+        let tripmaps = generate_tripmaps(1);
+        assert_eq!(tripmaps, vec![
+            vec![0],
+            vec![1],
+            vec![2]
+        ]);
+    }
+    #[test]
+    fn test_generate_tripmaps_2() {
+        let tripmaps = generate_tripmaps(2);
+        assert_eq!(tripmaps, vec![
+            vec![0, 0],
+            vec![0, 1],
+            vec![0, 2],
+            vec![1, 0],
+            vec![1, 1],
+            vec![1, 2],
+            vec![2, 0],
+            vec![2, 1],
+            vec![2, 2],
+        ]);
     }
 }
